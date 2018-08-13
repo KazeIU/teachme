696db837b0c8c7a614e961dd053c6293da9bdcd0
Fumiko Toyoda <toyodafumiko@gmail.com>
2018-08-13 17:58:15 +0900

    added validations to the models

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,5 @@
 class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :lesson
+  validates :start_time, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :lessons
-  has_many :appointments
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  has_many :lessons, dependent: :destroy
+  has_many :appointments, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
