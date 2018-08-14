3db4bb884aae335b8f68cf50aecf9c4e7b523034
Fumiko Toyoda <toyodafumiko@gmail.com>
2018-08-14 11:51:04 +0900

    deleted unnecessary validations from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+
   validates :email, presence: true, uniqueness: true
   has_many :lessons, dependent: :destroy
   has_many :appointments, dependent: :destroy
