ab59f88fa3dc8b836eddaa00297ff0fcc763e7a1
takumanaruke <leon32taku@gmail.com>
2018-08-16 12:48:20 +0900

    incremented a serch bar

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,11 @@
 class Lesson < ApplicationRecord
+  include PgSearch
+  pg_search_scope :search_by_location_and_language,
+    against: [ :location, :language ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
+
   belongs_to :user
   validates :language, inclusion: { in: ["English", "Japanese", "French"]}
   validates :level, inclusion: { in: ["Novice", "Conversational", "Business", "Fluent"]}
