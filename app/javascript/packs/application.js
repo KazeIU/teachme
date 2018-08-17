de4b63e8f2f17895e27ff0f4b18d0a3dba93ec49
Fumiko Toyoda <toyodafumiko@gmail.com>
2018-08-17 15:20:17 +0900

    typed.js on banner, and message on navbar deleted

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,6 @@
 import "bootstrap";
+import { loadDynamicBannerText } from '../components/banner';
+loadDynamicBannerText();
 // import { initUpdateNavbarOnScroll } from '../components/navbar';
 // initUpdateNavbarOnScroll();
 
